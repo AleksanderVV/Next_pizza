25503f3f8c3ae962fe09f43044ce33e731937fc4
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-03-07 12:46:27 +0200

    create button component

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <Button variant="outline">Click me</Button>
-    </div>
+    <main className="min-h-screen bg-white rounded-3xl">
+      <h1 className="text-3xl font-bold text-center mt-8">Next Pizza</h1>
+      <div className="flex justify-center mt-8">
+        <Button variant="outline">Order Now</Button>
+      </div>
+    </main>
   );
 }
