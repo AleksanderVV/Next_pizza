0c465512b1f700a4d77e54383363bf2fd3791a4c
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-03-07 13:05:07 +0200

    changed some classes

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,11 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-white rounded-3xl">
-      <h1 className="text-3xl font-bold text-center mt-8">Next Pizza</h1>
+    <div className="bg-white rounded-3xl p-8">
+      <h1 className="text-3xl font-bold text-center">Next Pizza</h1>
       <div className="flex justify-center mt-8">
         <Button variant="outline">Order Now</Button>
       </div>
-    </main>
+    </div>
   );
 }
