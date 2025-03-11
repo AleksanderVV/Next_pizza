622ff61e51fb102e0e5ffc5c9ad07b35973013b5
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-03-11 20:30:42 +0200

    create header starter component

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ export default function Home() {
   return (
     <div className="bg-white rounded-3xl p-8">
       <h1 className="text-3xl font-bold text-center">Next Pizza</h1>
-      <div className="flex justify-center mt-8">
-        <Button variant="outline">Order Now</Button>
-      </div>
     </div>
   );
 }
