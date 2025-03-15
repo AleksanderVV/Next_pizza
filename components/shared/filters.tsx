beffbe46dc172d71e6ade42dd2ebf85277d0c96a
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-03-15 14:29:22 +0200

    added price filters

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import { Title, FilterCheckbox } from "./index";
+import { Input } from "../ui";
 
 interface Props {
     className?: string;
@@ -10,9 +11,18 @@ export const Filters: React.FC<Props> = ({ className }) => {
     return (
         <div className={className}>
             <Title text="Filters" size="sm" className="mb-5 font-bold" />
+
             <div className="flex flex-col gap-4">
                 <FilterCheckbox text="Create your pizza" value="1" />
                 <FilterCheckbox text="New products" value="2" />
+            </div>
+
+            <div className="mt-5 border-y border-y-neutral-100 pt-6 pb-7">
+                <p className="font-bold mb-3">Price:</p>
+                <div className="flex gap-3 mb-5">
+                    <Input type="number" placeholder="0" min={0} max={30000} defaultValue={0} />
+                    <Input type="number" placeholder="30000" min={100} max={30000} />
+                </div>
             </div>
         </div>
     )
