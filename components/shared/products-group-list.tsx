0976958c6f00b8fe1c8a40a40c4845b70de96d80
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-03-19 14:23:13 +0200

    used react-use library for scroll effect

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import { cn } from '@/lib/utils';
 import React from 'react';
 import { Title } from './title';
 import { ProductCard } from './product-card';
+import { useIntersection } from 'react-use';
 
 interface Props {
     title: string;
@@ -12,8 +15,20 @@ interface Props {
 }
 
 export const ProductsGroupList: React.FC<Props> = ({ title, items, categoryId, className, listClassName }) => {
+    
+    const intersectionRef = React.useRef(null);
+    const intersection = useIntersection(intersectionRef, {
+        threshold: 0.4,
+    });
+
+    React.useEffect(() => {
+        if (intersection?.isIntersecting) {
+            console.log(title, categoryId);
+        }
+    }, [categoryId, intersection?.isIntersecting, title]);
+
      return (
-        <div className={className}>
+        <div className={className} id={title} ref={intersectionRef}>
             <Title text={title} size="lg" className="font-extrabold mb-5" />
 
             <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
