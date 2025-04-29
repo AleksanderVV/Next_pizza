ed92cd3880f4a31486fca7ddac3b7fbdc4682615
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-04-29 15:45:36 +0300

    created seed.ts for users

---
+++ prisma/seed.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from "@prisma/client";
+import { hashSync } from "bcrypt";
+
+const prisma = new PrismaClient();
+
+async function up() {
+    await prisma.user.createMany({
+        data: [
+            {
+                fullName: 'User',
+                email: 'user@test.ua',
+                password: hashSync('111111', 10),
+                verified: new Date(),
+                role: 'USER',
+            },
+            {
+                fullName: 'Admin',
+                email: 'admin@test.ua',
+                password: hashSync('111111', 10),
+                verified: new Date(),
+                role: 'ADMIN',
+            }
+        ]
+    })
+}
+
+async function down() {
+    await prisma.$executeRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE`;
+}
+
+async function main() {
+    try {
+        await down();
+        await up();
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+main().then(async () => {
+    await prisma.$disconnect();
+})
+.catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+})
