788d188de897801f69a4125dff9834e2b043729a
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-05-14 13:55:32 +0300

    fixed categories Link and add smooth scroll

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { cn } from "@/lib/utils";
 import { useCategoryStore } from "@/store/category";
 import { Category } from "@prisma/client";
+import Link  from "next/link";
 
 interface Props {
     items: Category[];
@@ -15,17 +16,28 @@ export const Categories: React.FC<Props> = ({ className, items }) => {
 
     const categoryActiveId = useCategoryStore((state) => state.activeId);
 
+    // Add smooth scroll
+    const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement>, name: string) => {
+        event.preventDefault();
+        const target = document.getElementById(name);
+        if (target) {
+            target.scrollIntoView({ behavior: "smooth" });
+        }
+        window.history.pushState(null, "", `/#${name}`);
+    };
+
     return (
         <div className={cn('inline-flex gap-1 bg-orange-50 p-1 rounded-2xl', className)}>
            {items.map(({ name, id }, index) => (
-                <a className={cn(
+                <Link className={cn(
                     'flex items-center font-bold h-11 rounded-2xl px-5',
                     categoryActiveId === id && 'bg-white shadow-md shadow-gray-300 text-primary')} 
                     key={index}
-                    href={`/#${name}`} 
+                    href={`/#${name}`}
+                    onClick={(event) => handleSmoothScroll(event, name)} 
                 >
                     {name}
-                </a>
+                </Link>
             ))}
         </div>
     )
