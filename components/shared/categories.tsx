e10738956cb22a045e879f80bfc12f15bbb22553
Oleksandr Voroniuk <voronyuk.aleksander@gmail.com>
2025-05-14 14:01:30 +0300

    fixed Link

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -16,28 +16,17 @@ export const Categories: React.FC<Props> = ({ className, items }) => {
 
     const categoryActiveId = useCategoryStore((state) => state.activeId);
 
-    // Add smooth scroll
-    const handleSmoothScroll = (event: React.MouseEvent<HTMLAnchorElement>, name: string) => {
-        event.preventDefault();
-        const target = document.getElementById(name);
-        if (target) {
-            target.scrollIntoView({ behavior: "smooth" });
-        }
-        window.history.pushState(null, "", `/#${name}`);
-    };
-
     return (
         <div className={cn('inline-flex gap-1 bg-orange-50 p-1 rounded-2xl', className)}>
            {items.map(({ name, id }, index) => (
-                <Link className={cn(
+                <a className={cn(
                     'flex items-center font-bold h-11 rounded-2xl px-5',
                     categoryActiveId === id && 'bg-white shadow-md shadow-gray-300 text-primary')} 
                     key={index}
-                    href={`/#${name}`}
-                    onClick={(event) => handleSmoothScroll(event, name)} 
+                    href={`#${name}`}
                 >
                     {name}
-                </Link>
+                </a>
             ))}
         </div>
     )
